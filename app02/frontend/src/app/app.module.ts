4f28a4f1efd7c8ffc3a7bcf8be46834a9107ec6d
Maximiliano Usich <maximiliano.usich@softvision.com>
2020-07-29 16:01:17 -0300

    angular, master detail

--- app02/frontend/src/app/app.module.ts
+++ app02/frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 
 import { FormsModule } from '@angular/forms';
+import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishDetailComponent
   ],
   imports: [
     BrowserModule,
